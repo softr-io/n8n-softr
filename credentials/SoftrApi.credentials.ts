adb4b12fcb1de3f7bcdecf15a000e97cb452caf3
Dariusz Mydlarz <dariusz@softr.io>
2025-08-13 13:13:16 +0200

    Fix icon display for Softr credentials

--- credentials/SoftrApi.credentials.ts
+++ credentials/SoftrApi.credentials.ts
@@ -1,15 +1,21 @@
 import {
-	IAuthenticateGeneric, IconRef,
+	IAuthenticateGeneric,
+	Icon,
+	IconFile,
 	ICredentialTestRequest,
 	ICredentialType,
 	INodeProperties,
+	Themed,
 } from 'n8n-workflow';
 
 export class SoftrApi implements ICredentialType {
 	name = 'softrApi';
 	displayName = 'Softr API';
 	documentationUrl = 'https://docs.softr.io';
-	icon: IconRef = 'file:softr.svg' as IconRef;
+	icon: Icon = {
+		light: 'file:softr.svg',
+		dark: 'file:softr.svg',
+	} as Themed<IconFile>;
 	properties: INodeProperties[] = [
 		{
 			displayName: 'API Key',
--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,19 @@ const { task, src, dest } = require('gulp');
 task('build:icons', copyIcons);
 
 function copyIcons() {
+	// copy node icons
 	const nodeSource = path.resolve('nodes', '**', '*.{png,svg}');
 	const nodeDestination = path.resolve('dist', 'nodes');
 
 	src(nodeSource).pipe(dest(nodeDestination));
 
+	// extra step: copy softr.svg into credential dist
+	const softrSource = path.resolve('nodes', 'Softr', 'softr.svg');
+	const softrDest = path.resolve('dist', 'credentials');
+	src(softrSource).pipe(dest(softrDest))
+
+	// copy credentials icons
 	const credSource = path.resolve('credentials', '**', '*.{png,svg}');
 	const credDestination = path.resolve('dist', 'credentials');
-
 	return src(credSource).pipe(dest(credDestination));
 }
